9024941288de7ade59cd4ad72a8e7714f7b8f91f
m0tl1ya <moriya.takayasu514@gmail.com>
2023-01-29 23:41:44 +0900

    manage input time with zustand

--- src/components/StartTimePicker.tsx
+++ src/components/StartTimePicker.tsx
@@ -6,16 +6,28 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
+import { create } from 'zustand'
+
+interface ClockState {
+  time: Dayjs | null
+  setTime: (value: Dayjs | null) => void
+}
+
+const useClockStore = create<ClockState>()((set) => ({
+  time: null,
+  setTime: (value) => set((state) => ({ time: value })),
+}))
+
 export default function StartTimePicker() {
-  const [value, setValue] = React.useState<Dayjs | null>(null);
+  const { time, setTime} = useClockStore();
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'ru'}>
       <TimePicker
         label="Basic example"
-        value={value}
+        value={time}
         onChange={(newValue) => {
-          setValue(newValue);
+          setTime(newValue);
         }}
         renderInput={(params) => <TextField {...params} />}
       />
