69e1b94882b53f59d479cee5f3a8f61f8d99f912
m0tl1ya <moriya.takayasu514@gmail.com>
2023-02-03 02:05:57 +0900

    format document

--- src/lib/store.tsx
+++ src/lib/store.tsx
@@ -3,25 +3,27 @@ import { create } from 'zustand'
 import { addTime } from './calculateTime'
 
 interface ClockState {
-  startTime: any | null
-  baseTime: any | null
-  breakTime: any | null
-  finishTime: any | null
-  setTime: (value: any | null) => void
-  setBaseTime: (value: any | null) => void
-  setBreakTime: (value: any | null) => void
-  setFinishTime: () => void
+    startTime: any | null
+    baseTime: any | null
+    breakTime: any | null
+    finishTime: any | null
+    setTime: (value: any | null) => void
+    setBaseTime: (value: any | null) => void
+    setBreakTime: (value: any | null) => void
+    setFinishTime: () => void
 }
 
 export const useClockStore = create<ClockState>()((set) => ({
-  startTime: "09:00",
-  baseTime: "07:45",
-  breakTime: "01:00",
-  finishTime: null,
-  setTime: (value) => set(() => ({ startTime: value })),
-  setBaseTime: (value) => set(() => ({ baseTime: value })),
-  setBreakTime: (value) => set(() => ({ breakTime: value })),
-  setFinishTime: () => set((state) => ({ finishTime: addTime(state.startTime, 
-    state.baseTime, state.breakTime)})),
+    startTime: "09:00",
+    baseTime: "07:45",
+    breakTime: "01:00",
+    finishTime: null,
+    setTime: (value) => set(() => ({ startTime: value })),
+    setBaseTime: (value) => set(() => ({ baseTime: value })),
+    setBreakTime: (value) => set(() => ({ breakTime: value })),
+    setFinishTime: () => set((state) => ({
+        finishTime: addTime(state.startTime,
+            state.baseTime, state.breakTime)
+    })),
 }))
 
