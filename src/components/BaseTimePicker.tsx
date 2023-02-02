9718fa398bc8787c1e914812fd7dbf3c5d131a8e
m0tl1ya <moriya.takayasu514@gmail.com>
2023-02-03 02:03:26 +0900

    bugfix: call wrong function

--- src/components/BaseTimePicker.tsx
+++ src/components/BaseTimePicker.tsx
@@ -5,14 +5,14 @@ import { useClockStore } from '@/lib/store';
 
 const BaseTimePicker: React.FC = () => {
     const { baseTime } = useClockStore();
-    const { setBreakTime } = useClockStore((state) => state);
+    const { setBaseTime } = useClockStore((state) => state);
 
     return (
         <TextField
             id="standard-basic"
             value={baseTime}
             onChange={(newValue) => {
-                setBreakTime(newValue.target.value);
+                setBaseTime(newValue.target.value);
             }}
             variant="standard" type={"time"} />
     );
